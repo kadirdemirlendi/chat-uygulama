667dbddf677ce33a6c45c9df879374799e790531
kadir demirlendi <kadir.demirlendi@gmail.com>
2020-02-26 14:43:00 +0300

    Socket.IO Redis Adapter Kurulumu

--- src/socketApi.js
+++ src/socketApi.js
@@ -8,6 +8,14 @@ const socketApi = {
 
 io.on('connection', socket => {
     console.log('bir kullanıcı giriş yaptı');
+    /**
+     * Redis Adapter
+     */
+    const redisAdapter = require('socket.io-redis');
+    io.adapter(redisAdapter({ 
+        host: process.env.REDIS_URI,
+        port: process.env.REDIS_PORT
+    }));
 });
 
 module.exports = socketApi;
