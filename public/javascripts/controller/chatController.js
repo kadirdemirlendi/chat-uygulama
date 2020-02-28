21a9c837c89845808ade894e70422575e244752a
kadir demirlendi <kadir.demirlendi@gmail.com>
2020-02-28 13:13:44 +0300

    251. Chat Odalarının Hazırlanması - 4 (Custom oda ismi vermek)

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -5,7 +5,7 @@ app.controller('chatController', ['$scope', ($scope) => {
      */
     $scope.onlineList = [];
     $scope.roomList = [];
-    $scope.activeTab = 2;
+    $scope.activeTab = 1;
 
     /**
      * socket.io event handling
@@ -22,8 +22,12 @@ app.controller('chatController', ['$scope', ($scope) => {
     });
 
     $scope.newRoom = () => {
-        let randomName = Math.random().toString(36).substring(7);
-        socket.emit('newRoom', randomName);
+        //let randomName = Math.random().toString(36).substring(7);
+
+        let roomName = window.prompt("Enter a room name");
+        if(roomName !== '' && roomName !== null){
+            socket.emit('newRoom', roomName);
+        }
     }
 
     $scope.changeTab = tab =>{
